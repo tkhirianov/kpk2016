8949df2e8920a5f0999fbee669744f8c0a1fe4ae
Timofey Khirianov <tkhirianov@gmail.com>
2016-07-05 19:06:15 +0300

    Немного исправил рисование графиков, чтобы воспользоваться масштабированием.

--- task_dm2.py
+++ task_dm2.py
@@ -1,12 +1,18 @@
 from drawman import *
 from time import sleep
 
+
 def f(x):
     return x*x
 
+print(drawman_scale)
+drawman_scale = 20
+x = -5.0
+to_point(x, f(x))
 pen_down()
-for x in range(0, 11):
+while x <= 5:
     to_point(x, f(x))
+    x += 0.1
 pen_up()
 
 sleep(10)
