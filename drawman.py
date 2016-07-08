62123f7dbc82af5e1f7d043dc874eaeb109fa91b
Татьяна <ganilovat@gmail.com>
2016-07-08 21:05:18 +0700

    Инициализация поля для построения графика 1. Выполняется запрос масштаба - число от 10 до 50 ( количество точек между делениями) 2. grid(default_schale) - Разлиновка поля горизонтальными и вертикальными линиями 3. axis(default_schale) - Прорисовка осей координат

--- drawman.py
+++ drawman.py
@@ -1,14 +1,68 @@
-from turtle import Turtle
-default_scale = 10
+import turtle
+
 
 def init_drawman():
     global t, x_current, y_current, _drawman_scale
-    t = Turtle()
-    t.penup()
-    x_current = 0
-    y_current = 0
-    t.goto(x_current, y_current)
+    default_scale = 0
+    while default_scale<10 or default_scale>50:
+            print('Введите число от 10 до 50 (масштаб координатной сетки)')
+            default_scale=int(input())
+    t=turtle.Turtle()
+    x_current=0
+    y_current=0
     drawman_scale(default_scale)
+    turtle.setup(20*default_scale,20*default_scale)#устанавливаем размеры экрана
+    grid(default_scale)
+    axis(default_scale)
+    x_current=0
+    y_current=0
+    t.goto(x_current,y_current)
+
+def axis(scale): #координатные оси ( масштаб)
+    # ось Х
+    pen_up()
+    t.goto(-scale*10, 0)
+    pen_down()
+    t.goto(scale*10-10,0)
+    # стрелочка на оси Х
+    x,y=t.pos()
+    t.begin_fill()
+    t.goto(x-10,y+10)
+    t.goto(x-10,y-10)
+    t.goto(x,y)
+    t.end_fill()
+    #  ось У
+    pen_up()
+    t.goto(0,-scale*10)
+    pen_down()
+    t.goto(0,scale*10-10)
+    # стрелочка на оси У
+    x,y=t.pos()
+    t.begin_fill()
+    t.goto(x-10,y-10)
+    t.goto(x+10,y-10)
+    t.goto(x,y)
+    t.end_fill()
+    pen_up()
+
+def grid(scale):    # рисуем сетку (масштаб)
+    t.color ('lightgray')
+    x=-10*scale # вертикальные
+    for k in range(20):
+        pen_up()
+        t.goto(x,-scale*10)
+        pen_down()
+        t.goto(x, scale*10)
+        x+=scale
+    y=10*scale # горизонтальные
+    for k in range(20):
+        pen_up()
+        t.goto(-scale*10, y)
+        pen_down()
+        t.goto(scale*10, y)
+        y-=scale
+    t.color('black')
+    pen_up()
 
 
 def drawman_scale(scale):
