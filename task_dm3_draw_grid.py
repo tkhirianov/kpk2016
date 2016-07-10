58f7b083572187fb7d0f112c8bfbc3febc42965e
acidulcer <acid_ulcer@inbox.ru>
2016-07-10 18:50:19 +0300

    Пример, демонстрирующий работу функции рисованя координатной сетки drawman_draw_grid(color_grid)

---
+++ task_dm3_draw_grid.py
@@ -0,0 +1,9 @@
+from drawman import *
+from time import sleep
+#прмиер работы команды drawman_draw_grid()
+#пока координатная сетка рисуется из нижнего левого угла
+
+drawman_scale(10)
+drawman_draw_grid('blue')
+
+sleep(4)
