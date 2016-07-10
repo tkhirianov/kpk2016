6ede00d0b8f06e9cd285058ea3d715daaa655835
acidulcer <acid_ulcer@inbox.ru>
2016-07-10 18:42:06 +0300

    Добавлена функиция рисования координатной сетки, учитывая коэффициент масштабирования Чертежника drawman_scale(scale) Координатные оси не рисует
    Функция drawman_draw_grid(color_grid) рисует кооринтаную сетку без осей, учитывая масштабирование Четрежника
    
        рисует кооринтаную сетку без осей
        запоминает размер окна поля
        x_width - ширина окна поля в пикселах
        y_height - высота окна поля в пикселах
        :param color_grid: цвет линий сетки указывается пользоватлем Чертежника при вызове команды drawman_draw_grid(color)
    
    Функция drawman_draw_Vline(x_width, y_height, color)  рисует вертикальные линии сетки, учитывая масштабирование Четрежника
    
        :param color: цвет линий сетки указывается пользоватлем Чертежника при вызове команды drawman_draw_grid(color_grid)
        :param x_width: ширина окна поля Чертежника, автоматически определяется при вызове команды drawman_draw_grid()
        :param y_height: высота окна поля Чертежника, автоматически определяется при вызове команды drawman_draw_grid()
    
     Фцнкция drawman_draw_Hline(x_width, y_height, color) рисует горизонтальные линии сетки, учитывая масштабирование Четрежника
    
         :param color: цвет линий сетки указывается пользоватлем Чертежника при вызове команды drawman_draw_grid(color)
        :param x_width: ширина окна поля Чертежника, автоматически определяется при вызове команды drawman_draw_grid()
        :param y_height: высота окна поля Чертежника, автоматически определяется при вызове команды drawman_draw_grid()

--- drawman.py
+++ drawman.py
@@ -27,6 +27,131 @@ def test_drawman():
     pen_up()
     to_point(0, 0)
 
+def drawman_draw_grid(color_grid):
+    """
+    рисует кооринтаную сетку без осей
+    запоминает размер окна поля
+    x_width - ширина окна поля в пикселах
+    y_height - высота окна поля в пикселах
+    :param color_grid: цвет линий сетки указывается пользоватлем Чертежника при вызове команды drawman_draw_grid(color)
+    :return:
+    """
+    x_width = t.screen.window_width()
+    y_height = t.screen.window_height()
+    drawman_draw_Hline(x_width, y_height, color_grid)
+    drawman_draw_Vline(x_width, y_height, color_grid)
+    pen_up()
+    to_point(0, 0)
+
+def drawman_draw_Vline(x_width, y_height, color):
+    """
+    рисование линий сетки из центра поля (т.е. из начальной позиции Чертежника)
+
+    рисует вертикальные линии сетки отельно по каждой четверти, учитывая масштабирование Четрежника
+
+    :param color: цвет линий сетки указывается пользоватлем Чертежника при вызове команды drawman_draw_grid(color)
+    :param x_width: ширина окна поля Чертежника, автоматически определяется при вызове команды drawman_draw_grid()
+    :param y_height: высота окна поля Чертежника, автоматически определяется при вызове команды drawman_draw_grid()
+    :return:
+    """
+    x = 0
+    y = 0
+    t.goto(x, y)
+    while x <= x_width//2:
+        t.pencolor(color)
+        t.goto(x, y)
+        t.pendown()
+        t.goto(x, y_height//2)
+        t.penup()
+        x = x + x_width//_drawman_scale
+
+    x = 0
+    y = 0
+    t.goto(x, y)
+    while x >= -x_width//2:
+        t.pencolor(color)
+        t.goto(x, y)
+        t.pendown()
+        t.goto(x, y_height//2)
+        t.penup()
+        x = x - x_width//_drawman_scale
+
+    x = 0
+    y = 0
+    t.goto(x, y)
+    while x >= -x_width//2:
+        t.pencolor(color)
+        t.goto(x, y)
+        t.pendown()
+        t.goto(x, -y_height//2)
+        t.penup()
+        x = x - x_width//_drawman_scale
+
+    x = 0
+    y = 0
+    t.goto(x, y)
+    while x <= x_width//2:
+        t.pencolor(color)
+        t.goto(x, y)
+        t.pendown()
+        t.goto(x, -y_height//2)
+        t.penup()
+        x = x + x_width//_drawman_scale
+
+
+def drawman_draw_Hline(x_width, y_height, color):
+    """
+    рисование линий сетки из центра поля (т.е. из начальной позиции Чертежника)
+
+    рисует горизонтальные линии сетки отельно по каждой четверти, учитывая масштабирование Четрежника
+
+    :param color: цвет линий сетки указывается пользоватлем Чертежника при вызове команды drawman_draw_grid(color)
+    :param x_width: ширина окна поля Чертежника, автоматически определяется при вызове команды drawman_draw_grid()
+    :param y_height: высота окна поля Чертежника, автоматически определяется при вызове команды drawman_draw_grid()
+    """
+    y = 0
+    x = 0
+    t.goto(x, y)
+    while y <= y_height//2:
+        t.pencolor(color)
+        t.goto(x, y)
+        t.pendown()
+        t.goto(x_width//2, y)
+        t.penup()
+        y = y + y_height//_drawman_scale
+
+    y = 0
+    x = 0
+    t.goto(x, y)
+    while y >= -y_height//2:
+        t.pencolor(color)
+        t.goto(x, y)
+        t.pendown()
+        t.goto(x_width//2, y)
+        t.penup()
+        y = y - y_height//_drawman_scale
+
+    y = 0
+    x = 0
+    t.goto(x, y)
+    while y >= -y_height//2:
+        t.pencolor(color)
+        t.goto(x, y)
+        t.pendown()
+        t.goto(-x_width//2, y)
+        t.penup()
+        y = y - y_height//_drawman_scale
+
+    y = 0
+    x = 0
+    t.goto(x, y)
+    while y <= y_height//2:
+        t.pencolor(color)
+        t.goto(x, y)
+        t.pendown()
+        t.goto(-x_width//2, y)
+        t.penup()
+        y = y + y_height//_drawman_scale
 
 def pen_down():
     t.pendown()
