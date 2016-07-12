a708a983dc8ceee8ae54a3f1fa13bf421e705058
Timofey Khirianov <tkhirianov@gmail.com>
2016-07-12 18:40:33 +0300

    Эксперименты с виджетами Tkinter.

--- catch_the_ball/ball.py
+++ catch_the_ball/ball.py
@@ -1,11 +1,11 @@
 import tkinter
 
+
 def button1_command():
     print('Button 1 default command.')
 
+
 def print_hello(event):
-    #print(event.char)
-    #print(event.keycode)
     print(event.num)
     print(event.x, event.y)
     #print(event.x_root, event.y_root)
@@ -18,14 +18,32 @@ def print_hello(event):
     else:
         raise ValueError()
 
-root = tkinter.Tk()
 
-button1 = tkinter.Button(root, text="Button 1", command=button1_command)
-button1.bind("<Button>", print_hello)
-button1.pack()
+def init_main_window():
+    """
+    Инициализация главного окна: создание всех необходимых виджетов и их упаковка.
+    :return:
+    """
+    global root, button1, button2, label, text, scale
 
-button2 = tkinter.Button(root, text="Button 2")
-button2.bind("<Button>", print_hello)
-button2.pack()
+    root = tkinter.Tk()
 
-root.mainloop()
+    button1 = tkinter.Button(root, text="Button 1", command=button1_command)
+    button1.bind("<Button>", print_hello)
+
+    button2 = tkinter.Button(root, text="Button 2")
+    button2.bind("<Button>", print_hello)
+
+    variable = tkinter.IntVar(0)
+    label = tkinter.Label(root, textvariable=variable)
+    scale = tkinter.Scale(root, orient=tkinter.HORIZONTAL, length=300,
+                          from_=0, to=100, tickinterval=10, resolution=5, variable=variable)
+    text = tkinter.Entry(root, textvariable=variable)
+
+    for obj in button1, button2, label, scale, text:
+        obj.pack()
+
+if __name__ == "__main__":
+    init_main_window()
+
+    root.mainloop()
