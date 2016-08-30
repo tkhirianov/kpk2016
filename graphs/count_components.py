1a6627dfd4192d3c6d84351c900e4e0cb138c8f8
Timofey Khirianov <tkhirianov@gmail.com>
2016-08-30 20:53:24 +0300

    count components

--- graphs/count_components.py
+++ graphs/count_components.py
@@ -1,22 +1,8 @@
 import networkx
 import matplotlib.pyplot as plt
 
-def input_edges_list():
-    """считывает список рёбер в форме:
-    в первой строке N - число рёбер,
-    затем следует N строк из двух слов и одного числа
-    слова - названия вершин, концы ребра, а число - его вес
-    
-    return граф в форме словаря рёбер и соответствующих им весов
-    """
-    N = int(input('Введите количество рёбер:'))
-    G = {}
-    for i in range(N):
-        vertex1, vertex2, weight = input().split()
-        weight = float(weight)
-        G[(vertex1, vertex2)] = weight
-    return G
-    
+color_list = ['red', 'green', 'blue', 'orange', 'magenta', 'pink', 'cyan']
+
 def edges_list_to_adjacency_list(E):
     """E - граф в форме словаря рёбер и соответствующих им весов
     return граф в форме словаря словарей смежности с весами
@@ -37,21 +23,25 @@ def edges_list_to_adjacency_list(E):
     return G
 
 
-def dfs(G, start, called = set(), skelet = set()):
+def dfs(G, start, called=set(), skelet=set()):
     called.add(start)
     for neighbour in G[start]:
         if neighbour not in called:
             dfs(G, neighbour, called, skelet)
             skelet.add((start, neighbour))
 
-
-
-s = """A B 1
+s = """L B 1
 B D 1
 B C 2
-C A 2
-C D 3
-D E 5""".split('\n')
+G J 7
+V W 2
+N M 2
+C L 2
+O D 3
+G R 1
+R T 2
+N S 2
+Y W 5""".split('\n')
 E = {}
 for line in s:
     a, b, weight = line.split()
@@ -59,19 +49,25 @@ for line in s:
 
 A = edges_list_to_adjacency_list(E)
 
-called = set()
-skelet = set()
-dfs(A, 'A', called, skelet)
-print(called)
-print(skelet)
-
 G = networkx.Graph(A)
 position = networkx.spring_layout(G) # positions for all nodes
 networkx.draw(G, position)
 networkx.draw_networkx_labels(G, position)
-networkx.draw_networkx_edge_labels(G, position, edge_labels=E)
-# нарисуем остовное дерево:
-networkx.draw_networkx_edges(G, position, edgelist=skelet,
-                             width=5, alpha=0.5, edge_color='red')
+
+called = set()
+count_components = 0
+for vertex in A:
+    if vertex in called:
+        continue
+    else:
+        # vertex ещё не встречалась ни в одной обойдённой компоненте связности
+        skelet = set()
+        dfs(A, vertex, called, skelet)
+        networkx.draw_networkx_edges(G, position, edgelist=skelet,
+                                     width=5, alpha=0.5,
+                                     edge_color=color_list[count_components])
+        count_components += 1
+
+print('Количество компонент связности:', count_components)
 
 plt.show() # display
